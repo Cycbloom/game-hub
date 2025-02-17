d2770c02ef61a4c362c65b2e4c8b7046e7580217
Cycbloom <1756138655@qq.com>
2025-02-17 11:43:02 +0800

    fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,20 @@
+import { Text, UnorderedList, ListItem } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+
+const GenreList = () => {
+  const { genres, error, loading } = useGenres();
+
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      {loading && <Text>Loading...</Text>}
+      <UnorderedList>
+        {genres.map((genre) => (
+          <ListItem key={genre.id}>{genre.name}</ListItem>
+        ))}
+      </UnorderedList>
+    </>
+  );
+};
+
+export default GenreList;
